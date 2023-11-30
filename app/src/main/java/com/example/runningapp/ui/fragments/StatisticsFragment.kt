d68c8f5469241661f0db8895d8111a28a2b1c50c
ahmeednasr <ahmedna7sr@gmail.com>
2023-11-30 16:40:30 +0200

    add BarChart

--- app/src/main/java/com/example/runningapp/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/StatisticsFragment.kt
@@ -1,15 +1,23 @@
 package com.example.runningapp.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.example.runningapp.R
 import com.example.runningapp.databinding.FragmentStatisticsBinding
+import com.example.runningapp.other.CustomMarkerView
 import com.example.runningapp.ui.viewmodels.StatisticViewModel
 import com.example.runningapp.util.TrackingUtility
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import dagger.hilt.android.AndroidEntryPoint
 import java.lang.Math.round
 import kotlin.math.roundToInt
@@ -33,6 +41,7 @@ class StatisticsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         subscribeToObservers()
+        setupBarChart()
     }
 
     private fun subscribeToObservers() {
@@ -62,6 +71,47 @@ class StatisticsFragment : Fragment() {
                 val totalCalories = "${it}kcal"
                 binding.tvTotalCalories.text = totalCalories
             }
+        }
+        viewModel.runsSortedByDate.observe(viewLifecycleOwner) {
+            it?.let {
+                val allAvgSpeed = it.indices.map { i ->
+                    BarEntry(i.toFloat(), it[i].avgSpeedInKMH)
+                }
+                val barDataSet = BarDataSet(allAvgSpeed, "Avg Speed Over Time").apply {
+                    valueTextColor = Color.WHITE
+                    color = ContextCompat.getColor(
+                        requireContext(),
+                        pub.devrel.easypermissions.R.color.colorAccent
+                    )
+                }
+                binding.barChart.data= BarData(barDataSet)
+                binding.barChart.marker=CustomMarkerView(it.reversed(),requireContext(),R.layout.marker_view)
+                binding.barChart.invalidate()
+            }
+        }
+    }
+
+    private fun setupBarChart() {
+        binding.barChart.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            setDrawLabels(false)
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        binding.barChart.axisLeft.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        binding.barChart.axisRight.apply {
+            axisLineColor = Color.WHITE
+            textColor = Color.WHITE
+            setDrawGridLines(false)
+        }
+        binding.barChart.apply {
+            description.text = "AvgSpeedOver"
+            legend.isEnabled = false
         }
     }
 
